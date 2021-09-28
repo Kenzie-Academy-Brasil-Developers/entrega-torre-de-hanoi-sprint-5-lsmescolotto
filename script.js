84e812f85e15209b948516d1c3e186a6b4409cb1
guilherme <guilherme.desoler@gmail.com>
2021-09-28 18:17:55 -0300

    Adicionando funcao q pega disco e posiciona em outra torre, uma variavel contador e div segurando o disco a ser posicionado.

--- script.js
+++ script.js
@@ -4,18 +4,22 @@ const tower3 = document.createElement('div')
 const start = document.getElementById('start')
 start.addEventListener('click', difficulty)
 
+let holding = 0
+let moves = 0
+
 function difficulty() {
     const numberPieces = document.getElementById('difficulty')
     const size = numberPieces.value
     console.log(size)
-    createTower(size)
     createSticks(size)
+    createTower(size)
 }
 
 
 function createTower(size) {
     const page = document.querySelector('main')
-    
+    const current = document.getElementById('current')
+    current.style = 'display: flex;'
     
     tower1.id = 'tower1'
     tower2.id = 'tower2'
@@ -24,14 +28,17 @@ function createTower(size) {
     page.appendChild(tower2)
     page.appendChild(tower3)
 
-    const colors = ['blue', 'green', 'yellow', 'violet', 'red', 'purple', 'black']
+    const colors = ['blue', 'green', 'yellow', 'violet', 'red', 'purple', 'gray']
 
     for(let i = 1; i <= size; i++) {
         let disc = document.createElement('div')
+        disc.innerText = (size - i) + 1
         disc.id = `disc${i}`
         disc.style = `width: ${50 + 25*(size-i)}px; height: 30px; background-color: ${colors[i-1]};`
         tower1.appendChild(disc)
     }
+    addEventToTower()
+    removeInterface()
 }
 
 
@@ -58,3 +65,43 @@ function victory() {
         console.log("that's a win!!!")
     }
 }
+
+function addEventToTower() {
+    tower1.addEventListener('click', selectPiece)
+    tower2.addEventListener('click', selectPiece)
+    tower3.addEventListener('click', selectPiece)
+}
+
+function selectPiece(evt) {
+    if(holding !== 1 && evt.currentTarget.childElementCount > 1) {
+        const current = document.getElementById('current')
+        const taken = evt.currentTarget.lastChild
+        current.appendChild(taken)
+        holding = 1
+    } else {
+        putPiece(evt)
+    }
+}
+
+function putPiece(evt) {
+    if(holding !== 0) {
+        const piece = document.getElementById('current').lastChild
+        const nextTower = evt.currentTarget
+        if(nextTower.childElementCount > 1) {
+            const lastNumber = nextTower.lastChild
+            if(piece.clientWidth < lastNumber.clientWidth) {
+                nextTower.appendChild(piece)
+                holding = 0
+            }
+        } else {
+            nextTower.appendChild(piece)
+            holding = 0
+        }
+    moves += 1
+    }
+}
+
+function removeInterface() {
+    const dif = document.getElementById('dif')
+    dif.style = 'display: none;'
+} 
